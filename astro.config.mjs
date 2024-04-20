7412f097203f913661b979d9666dac470346a693
Gabriel Morandi <gabriel.morandi@intelitrader.com.br>
2024-04-20 01:43:33 -0300

    Revert "add vercel"
    This reverts commit 124f7905c541ddd1e5a40315c884a095af06538c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   image: {
-    domains: ["astro.build"]
+    domains: ["astro.build"],
   },
-  output: "server",
-  adapter: vercel()
-});
+})
